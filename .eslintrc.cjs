d6dc3cf83834dda189bdfa2f2e2e426f923f24e2
Makino Shoji <s-makino@springboard-inc.jp>
2023-05-22 15:08:15 +0900

    🎉 Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,20 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    "no-multi-spaces": ["error"],
+    "vue/no-multiple-template-root": "off",
+    "no-unused-vars": "off",
+    "no-use-before-define": "off"
+  }
+}
