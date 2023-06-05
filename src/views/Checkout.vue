64354122cd728a51604bd49c208fcdaf5322d103
Makino Shoji <s-makino@springboard-inc.jp>
2023-06-05 16:13:18 +0900

    🚧 Change Bootstrap to Tailwind CSS

--- src/views/Checkout.vue
+++ src/views/Checkout.vue
@@ -3,8 +3,7 @@
   <div class="container">
     <h1>カート</h1>
 
-    <!-- <table class="table"> -->
-    <table class="table-auto">
+    <table class="table-auto w-full">
       <caption class="text-right h3">
         <b>合計:</b>
         <curr :amt="Number(cartTotal)"></curr>
@@ -13,7 +12,7 @@
         <tr class="border-b-2 border-slate-200">
           <th scope="col"></th>
           <th scope="col">品名</th>
-          <th scope="col" class="text-center">数量</th>
+          <th scope="col" class="text-center w-30">数量</th>
           <th scope="col" class="text-right">価格</th>
           <th scope="col" class="text-right">小計</th>
         </tr>
@@ -22,10 +21,10 @@
         <tr v-for="(item, index) in cart" :key="item.product.id" class="border-b-2 border-slate-200 hover:bg-slate-300">
           <td class="p-2">
             <div class="flex" role="group" aria-label="Basic example">
-              <button @click="this.$emit('addItem', item.product)" class="px-2 py-2 bg-purple-500 text-white hover:bg-purple-600">
+              <button @click="this.$emit('addItem', item.product)" class="px-2 py-2 bg-slate-200 text-black hover:bg-slate-400 border-2 border-slate-300 rounded-l-lg">
                 +
               </button>
-              <button @click="this.$emit('deleteItem', index)" class="px-2 py-2 hover:bg-purple-500 hover:text-white">
+              <button @click="this.$emit('deleteItem', index)" class="px-2 py-2 bg-slate-200 text-black hover:bg-slate-400 border-2 border-slate-300 rounded-r-lg">
                 -
               </button>
             </div>
@@ -37,7 +36,7 @@
         </tr>
       </tbody>
     </table>
-    <router-link class="btn btn-sm btn-success" to="/">買い物を続ける</router-link>
+    <router-link class="px-2 py-2 bg-purple-500 text-white hover:bg-purple-600 rounded-lg no-underline" to="/">買い物を続ける</router-link>
   </div>
 </template>
 
